71f5926698c9a3e4b35be17d7b6218783800779f
Firdaus Adib <i@firdaus.my>
2013-10-04 21:26:43 +0800

    Omitted Uservoice

--- templates/offiria/index.php
+++ templates/offiria/index.php
@@ -782,15 +782,7 @@ $(function() {
 });
 </script>
 <?php ##CUTSTART ?>
-<!-- uservoice -->
-<script type="text/javascript">
-  var uvOptions = {};
-  (function() {
-    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
-    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/yxQEHUXVPLvK5kQDwmd7XQ.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
-  })();
-</script>
+
 <?php ##CUTSTOP ?>
 <jdoc:include type="modules" name="analytics" style="default"/>
 </body>
