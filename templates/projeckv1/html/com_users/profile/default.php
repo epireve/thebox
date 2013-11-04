7d552e58e02699295d1b58af5daa5c5540763a0c
Firdaus Adib <i@firdaus.my>
2013-11-05 04:41:34 +0800

    Removed dummy template

--- templates/projeckv1/html/com_users/profile/default.php
+++
@@ -1,7 +0,0 @@
-<?php
-// if theres another session already exist
-// Forgot Username | Forgot Password will redirect the request to this page
-// redirect them to home page
-$mainframe = JFactory::getApplication();
-$mainframe->redirect(JRoute::_('index.php?option=com_stream&view=company'));
-?>
